93275a2f2a7a989c7fa6c67ea5a21b0158724362
varshini-vars <varshuvijayakumar@gmail.com>
2025-03-02 09:50:37 +0530

    adding all files

---
+++ server.js
@@ -0,0 +1,68 @@
+const express=require('express')
+const mongoose=require('mongoose')
+require('dotenv').config()
+const User=require('./models/User')
+const bcrypt=require('bcryptjs')
+
+const app=express()
+const PORT=3700
+app.use(express.json());
+
+app.get('/',(req, res)=>{
+    res.send("<h1 align=center>WELCOME TO RECIPE FOLIO</h1>")
+})
+
+app.post('/register',async(req, res)=>{
+    const {username,email,password}=req.body
+    try{
+        const hashedPassword= await bcrypt.hash(password,10)
+        const user=new User({username,email,password:hashedPassword})
+        await user.save()
+        res.json({message: "User Registred.."})
+        console.log("User Registration completed...")
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+})
+
+
+
+
+app.post('/login',async(req,res)=>{
+    const {email,password}=req.body
+    try{
+        const user = await User.findOne({ email });
+        if (!user || !(await bcrypt.compare(password, user.password))) 
+            {
+             return res.status(400).json({ message: "Invalid Credentials" });
+            }
+          res.json({ message: "Login Successful", username: user.username });
+    }
+    
+    catch(err)
+    {
+        console.log(err)
+    }
+})
+
+
+
+mongoose.connect(process.env.MONGO_URL).then(
+    ()=>console.log("DB connected successfully..")
+).catch(
+    (err)=>console.log(err)
+)
+
+    
+
+
+
+app.listen(PORT,(err)=>{
+    if(err)
+    {
+        console.log(err)
+    }
+    console.log("Server is running on port :"+PORT)
+})
